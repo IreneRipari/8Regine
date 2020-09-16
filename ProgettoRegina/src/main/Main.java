1e1846406da4a91a22007c0245ec15267ccaedea
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-09-16 16:27:23 +0200

    fine

--- ProgettoRegina/src/main/Main.java
+++ ProgettoRegina/src/main/Main.java
@@ -1,15 +1,28 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
 		Soluzioni soluzione= new Soluzioni();
 		Queen[] queen = new Queen[8];
+		List<Queen> threadList =  new ArrayList<Queen>();
+		int threadId = 0;
+		
 		for (int i = 0; i < 8; i++) {
 			queen[i] = new Queen(i,soluzione);
 			queen[i].start();
+			threadList.add(queen[i]);
+			threadId++;
 		}
-		while (soluzione.getSize()!=8) {
-			
+		for(Queen q : threadList) {
+			try {
+				q.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		soluzione.mostraSoluzioni();
 
